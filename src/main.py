6f8b913c378015eb888b4ef71cfc4391e1804dbc
Gossepojke <svedlund.oskar@gmail.com>
2022-09-23 12:35:37 +0200

    start

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
+#Oskar Svedlund & Oliver stafferod
+#TEINF-20
+#2022-09-23
+#Internal Python Chat projekt
+
 import socket
+
---
+++ src/resources.py
@@ -0,0 +1,4 @@
+#Oskar Svedlund & Oliver stafferod
+#TEINF-20
+#2022-09-23
+#Internal Python Chat projekt
