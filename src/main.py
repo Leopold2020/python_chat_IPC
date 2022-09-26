da7075fdd945ade4e1041f72f30d3b1b93866ec0
Gossepojke <svedlund.oskar@gmail.com>
2022-09-26 09:08:31 +0200

    started to create something

--- src/main.py
+++ src/main.py
@@ -4,4 +4,16 @@
 #Internal Python Chat projekt
 
 import socket
+from threading import Thread
+from resources import listen_for_client
 
+Server_host = "0.0.0.0"
+Server_port = 5002
+
+
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((Server_host, Server_port))
+s.listen(5)
+
+print(f"[...] Listening as {Server_host}:{Server_port}")
--- src/resources.py
+++ src/resources.py
@@ -2,3 +2,25 @@
 #TEINF-20
 #2022-09-23
 #Internal Python Chat projekt
+
+import socket
+from threading import Thread
+
+client_sockets = set()
+Separator_token = "<SEP>"
+
+
+def listen_for_client(cs):
+     while True:
+        try:
+            # keep listening for a message from `cs` socket
+            msg = cs.recv(1024).decode()
+        except Exception as e:
+            # client no longer connected
+            # remove it from the set
+            print(f"[!] Error: {e}")
+            client_sockets.remove(cs)
+        else:
+            # if we received a message, replace the <SEP> 
+            # token with ": " for nice printing
+            msg = msg.replace(Separator_token, ": ")
